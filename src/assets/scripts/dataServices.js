c13859b10de53a934a2fa362a7ebf4a4c6f2ac51
Souhail RAZZOUK <souhail_27@hotmail.fr>
2016-04-09 01:51:55 +0100

    Corrected URLs and refined dataServices javascript

--- src/assets/scripts/dataServices.js
+++ src/assets/scripts/dataServices.js
@@ -11,74 +11,23 @@
   
   usersServices.factory('UserDetails', ['$resource', 
     function($resource) {
-      return $resource('/users.json', {}, {
+      return $resource('https://api.github.com/users', {}, {
                 query:{method:'GET', params:{userLogin:"@login"}, isArray: true}
               });
     }]);
     
   usersServices.factory('UserFollowers', ['$resource', 
     function($resource) {
-        return $resource('/followers.json', {
+        return $resource('https://api.github.com/users/:userLogin/followers', {
                 query:{method:'GET', params:{userLogin:"@login"}, isArray: true}
               });
     }]);
 
   usersServices.factory('UserFollowing', ['$resource', 
     function($resource) {
-        return $resource('/following.json', {
+        return $resource('https://api.github.com/users/:userLogin/following', {
                 query:{method:'GET', params:{userLogin:"@login"}, isArray: true}
               });
     }]);
     
-  usersServices.factory('UserSocialNetwork', ['$resource', 
-    function($resource) {
-      var socials = {},
-          srvc = $resource('/:socialType.json', {
-              query:{method:'GET', params:{socialType:"@followers"}, isArray: true}
-            });
-            
-        socials.followersList = [];
-        socials.followingList = [];
-        
-        srvc.query({socialType:'followers'}).$promise.then(function(data) {
-          socials.followersList = data;
-        });
-        
-        srvc.query({socialType:'following'}).$promise.then(function(data) {
-          socials.followingList = data;
-        });
-        
-        return socials;
-    }]);
 })();
-
-/*
-
-app.factory('NoteResource', ['$resource',
-  function($resource) {
-    var res =  $resource('http://okigan.apiary.io/notes/:id', {}, {
-      query: {
-        method: 'GET',
-        params: {
-        },
-        isArray: true,
-        transformResponse: function(data, header){
-          //Getting string data in response
-          var jsonData = JSON.parse(data); //or angular.fromJson(data)
-          var notes = [];
-
-          angular.forEach(jsonData, function(item){
-            var note = new Note();
-            note.noteTitle = item.title;  
-            notes.push(note);
-          });
-
-          return notes;
-        }
-      }
-    });
-    return res;
-  }
-]);
-
- */
